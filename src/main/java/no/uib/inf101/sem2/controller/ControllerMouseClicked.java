28487f1e2fa955e8c1e166c9eba6edfde8cef127
git <git>
2023-04-05 20:15:42 +0200

    den hopper!

--- src/main/java/no/uib/inf101/sem2/controller/ControllerMouseClicked.java
+++ src/main/java/no/uib/inf101/sem2/controller/ControllerMouseClicked.java
@@ -1,6 +1,10 @@
 package no.uib.inf101.sem2.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import javax.swing.Timer;
 import no.uib.inf101.sem2.model.Playermodel;
 import no.uib.inf101.sem2.model.RandomThrow;
 import no.uib.inf101.sem2.view.GridView;
@@ -9,12 +13,21 @@ public class ControllerMouseClicked implements MouseListener {
   private final Playermodel model;
   private final GridView view;
   private RandomThrow randomThrow = new RandomThrow();
+  private int diceEyesToAnimate = 0;
+  private Timer animationTimer;
+  private static final int ANIMATION_DELAY_MS = 100;
+  private GameState gameState = GameState.PLAY;
 
   public ControllerMouseClicked(Playermodel model, GridView view) {
     this.model = model;
     this.view = view;
     this.view.addMouseListener(this);
-  
+    this.animationTimer = new Timer(ANIMATION_DELAY_MS, new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        animateSingleStep();
+      }
+    });
   }
 
   @Override
@@ -22,28 +35,47 @@ public class ControllerMouseClicked implements MouseListener {
     if(view.getDiceRectangle().contains(e.getPoint())){
       int eyes = randomThrow.rollDice();
       model.updateDiceNumber(eyes);
-      for (int i = 0; i < eyes; i++) {
-        model.PlayerJump();
-        view.repaint();
-        try {
-              Thread.sleep(100);
-          } catch (InterruptedException e1) {
-              e1.printStackTrace();
-          }
-      }
-      // model.SteppedOnLadder();
-      // model.SteppedOnSnake();
+      diceEyesToAnimate = eyes;
+      gameState = GameState.ANIMATE;
+      animationTimer.start();
     }
     view.repaint();
   }
 
-  @Override 
-public void mousePressed(MouseEvent e) { //når musen trykkes innenfor view.dice
-  /* ignore */ }
-  @Override public void mouseReleased(MouseEvent e) { /* ignore */ }
-  @Override 
-  public void mouseEntered(MouseEvent e) { //når musen er innenfor view.dice
-    /* ignore */ }
-  @Override public void mouseExited(MouseEvent e) { /* ignore */ }
+  private void animateSingleStep() {
+    if (diceEyesToAnimate > 0) {
+      model.PlayerJump();
+      diceEyesToAnimate--;
+      view.repaint();
+    } else {
+      model.SteppedOnLadder();
+      gameState = GameState.PLAY;
+      animationTimer.stop();
+    }
+  }
 
+  @Override
+  public void mousePressed(MouseEvent e) {
+    // ignore
+  }
+
+  @Override
+  public void mouseReleased(MouseEvent e) {
+    // ignore
+  }
+
+  @Override
+  public void mouseEntered(MouseEvent e) {
+    // ignore
+  }
+
+  @Override
+  public void mouseExited(MouseEvent e) {
+    // ignore
+  }
+
+  private enum GameState {
+    PLAY,
+    ANIMATE
+  }
 }
--- src/main/java/no/uib/inf101/sem2/model/GameState.java
+++ src/main/java/no/uib/inf101/sem2/model/GameState.java
@@ -2,5 +2,5 @@ package no.uib.inf101.sem2.model;
 
 public enum GameState {
     GameOver,
-    GameActive
+    GameActive,
 }
