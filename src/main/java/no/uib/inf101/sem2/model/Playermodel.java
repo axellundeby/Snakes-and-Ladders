e4064ca240cfdb416f1f1aabf2a85502ac4fdfb9
git <git>
2023-04-17 10:04:10 +0200

    mjau

--- src/main/java/no/uib/inf101/sem2/model/Playermodel.java
+++ src/main/java/no/uib/inf101/sem2/model/Playermodel.java
@@ -1,7 +1,6 @@
 package no.uib.inf101.sem2.model;
+import java.util.Arrays;
 import java.util.List;
-
-import no.uib.inf101.sem2.controller.ControllerMouseClicked;
 import no.uib.inf101.sem2.grid.CellPosition;
 import no.uib.inf101.sem2.grid.GridCell;
 import no.uib.inf101.sem2.grid.GridDimension;
@@ -15,7 +14,6 @@ public class Playermodel implements ViewableModel, PlayerFactory{
     
     private int PlayerListIndex = 0;
     private List<Player> PlayerList;
-    private Player player;
     
      
 //currentPlayer = playerList.get(playerListIndex % playerList.size());
@@ -24,12 +22,10 @@ public class Playermodel implements ViewableModel, PlayerFactory{
 // set num players(int)
 // hente liste fra factory med spillere - gjort?
 
-    public Playermodel(Board board, PlayerFactory factory, Player player){
+    public Playermodel(Board board, PlayerFactory factory){
         this.board = board;
         this.factory = factory;
-        this.PlayerList = factory.getPlayerList();
-        this.player = PlayerList.get(PlayerListIndex);
-        
+        this.PlayerList = factory.getPlayerList(); 
     }
 
     @Override
@@ -43,7 +39,7 @@ public class Playermodel implements ViewableModel, PlayerFactory{
     }
     @Override
     public Iterable<GridCell<Character>> getPiece() {
-       return player;
+       return PlayerList.get(PlayerListIndex);
     }
 
     @Override
@@ -78,21 +74,22 @@ public class Playermodel implements ViewableModel, PlayerFactory{
    
 
     private void movePlayer(int MoveToRow, int MoveTocol) {
-        Player ShapeCopy = player.shiftedBy(MoveToRow, MoveTocol);
-        this.player = ShapeCopy;
+        Player ShapeCopy = PlayerList.get(PlayerListIndex).shiftedBy(MoveToRow, MoveTocol);
+        PlayerList.set(PlayerListIndex,ShapeCopy);
+        
+    }
     
-    }
-
     private void movePlayerTo(int MoveToRow, int MoveToCol) {
-        player.setPos(new CellPosition(MoveToRow, MoveToCol));
+        PlayerList.get(PlayerListIndex).setPos(new CellPosition(MoveToRow, MoveToCol));
     }
-
+    
+    //PlayerList.get(PlayerListIndex).setPos(ShapeCopy.getPos());
     /**
      * This method moves the player right if the row is even and left if it is odd, if the player is on the edge of the board it moves the player up.
      */
     public void PlayerJump() {
-        int PlayerRow = player.getPos().row();
-        int PlayerCol = player.getPos().col();
+        int PlayerRow = PlayerList.get(PlayerListIndex).getPos().row();
+        int PlayerCol = PlayerList.get(PlayerListIndex).getPos().col();
         if(PlayerRow % 2 != 0){
             if (PlayerCol == 9){
                 PlayerOnEdge();
@@ -129,72 +126,59 @@ public class Playermodel implements ViewableModel, PlayerFactory{
      */
 
     public void SteppedOnSnake() {
-        int row = player.getPos().row();
-        int col = player.getPos().col();
+        int row = PlayerList.get(PlayerListIndex).getPos().row();
+        int col = PlayerList.get(PlayerListIndex).getPos().col();
 
         if (row == 0 && col == 1) {
             movePlayerTo(5, 0);
-            gameInfo = GameInfo.SNAKE;
         } else if (row == 1 && col == 8) {
             movePlayerTo(4, 7);
-            gameInfo = GameInfo.SNAKE;
         } else if (row == 2 && col == 4) {
             movePlayerTo(4, 2);
-            gameInfo = GameInfo.SNAKE;
         } else if (row == 3 && col == 5) {
             movePlayerTo(5, 4);
-            gameInfo = GameInfo.SNAKE;
         } else if (row == 4 && col == 6) {
              movePlayerTo(6, 9);
-             gameInfo = GameInfo.SNAKE;
         } else if (row == 5 && col == 2) {
             movePlayerTo(9, 2);
-            gameInfo = GameInfo.SNAKE;
         } else if (row == 6 && col == 0) {
             movePlayerTo(9, 2);
-            gameInfo = GameInfo.SNAKE;
         } else if (row == 7 && col == 6) {
             movePlayerTo(9, 4);
-            gameInfo = GameInfo.SNAKE;
         }
+        gameInfo = GameInfo.SNAKE;
      }
 
     /**
      * This method checks if the player is on a ladder and moves the player accordingly.
      */
      public void SteppedOnLadder() {
-        int row = player.getPos().row();
-        int col = player.getPos().col();
+        int row = PlayerList.get(PlayerListIndex).getPos().row();
+        int col = PlayerList.get(PlayerListIndex).getPos().col();
         if (row == 9 && col == 3) {
             movePlayerTo(7, 4);
-            gameInfo = GameInfo.LADDER;
         } else if (row == 8 && col == 7) {
             movePlayerTo(5, 5);
-            gameInfo = GameInfo.LADDER;
         } else if (row == 6 && col == 7) {
             movePlayerTo(5, 8);
-            gameInfo = GameInfo.LADDER;
         } else if (row == 5 && col == 1) {
             movePlayerTo(3, 2);
-            gameInfo = GameInfo.LADDER;
         } else if (row == 5 && col == 9) {
             movePlayerTo(3, 8);
-            gameInfo = GameInfo.LADDER;
         } else if (row == 3 && col == 1) {
             movePlayerTo(1, 0);
-            gameInfo = GameInfo.LADDER;
         } else if (row == 2 && col == 6) {
             movePlayerTo(0, 8);
-            gameInfo = GameInfo.LADDER;
         }
+        gameInfo = GameInfo.LADDER;
     }
     /**
      * this method checks if the player is out of the grid if so, the player wins.
      */
 
     public void Winner() {
-        int row = player.getPos().row();
-        int col = player.getPos().col();
+        int row = PlayerList.get(PlayerListIndex).getPos().row();
+        int col = PlayerList.get(PlayerListIndex).getPos().col();
         if(row<0 || col<0){
             gameInfo = GameInfo.WINNER;
             gameState=GameState.disbaleDice;
@@ -208,7 +192,7 @@ public class Playermodel implements ViewableModel, PlayerFactory{
     public void PlayerAppear(){
         if(!factory.hasMorePlayers()){
             Player CurrentPlayerTemp = factory.getNext();
-            player = CurrentPlayerTemp;
+            PlayerList.set(PlayerListIndex,CurrentPlayerTemp);
         }
     }
     //viljar sa noe om rar indeksering hopping, hente ut amout of players
@@ -216,11 +200,13 @@ public class Playermodel implements ViewableModel, PlayerFactory{
      * This method chenges the players turn, by changing the index of the player in the playerlist.
      */
     public void PlayerTurn(){
-        if (PlayerListIndex >= ControllerMouseClicked.getamountOfPlayers() - 1) {//er dette lov
+        if (PlayerListIndex >= 3) {//er dette lov, helst amountOfPlayers
             PlayerListIndex = 0;
+
         } else {
             PlayerListIndex++;
-            player = PlayerList.get(PlayerListIndex);//tipper denne er riktig
+            PlayerList.get(PlayerListIndex);//tipper denne er feil
+            //PlayerList.set(PlayerListIndex,PlayerList.get(PlayerListIndex));
         }
     }
 
@@ -233,16 +219,53 @@ public class Playermodel implements ViewableModel, PlayerFactory{
     //om en spiller går på en annen spiller, flyttes den til en tilfeldig posisjon under rad 7
     //sjekket at jeg ikke gikk på meg selv
     //hvis det ligger en spiller som ikke er seg selv, flytt den spilleren til blabla, bruk en for loop for å hente alle spillere
+    //hopp over plassen til en annen spiller, behold stump
     //må da hente ut spillerID fra listen over
     public void stumpPlayer() {
         int randomrow = (int) (Math.random() * 3) + 7;
         int randomCol = (int) (Math.random() * 10);
-        CellPosition pos = player.getPos();
+        CellPosition pos = PlayerList.get(PlayerListIndex).getPos();
         if(board.get(pos) == '-'){
             movePlayer(randomrow, randomCol);
             gameInfo = GameInfo.STUMP;
         }
     }
+
+    private void playerOnNextTile(){
+        int PlayerRow = PlayerList.get(PlayerListIndex).getPos().row();
+        int PlayerCol = PlayerList.get(PlayerListIndex).getPos().col();
+
+        if(PlayerRow % 2 == 0){
+            if(PlayerCol == 0){
+                movePlayer(1, -1);
+            }
+            movePlayer(0, -2);
+        }
+        else if(PlayerRow % 2 != 0){
+            if(PlayerCol == 9){
+                movePlayer(1, 1);
+            }
+            movePlayer(0, 2);
+        }
+
+    }
+
+    public boolean playerOnNextTileChecker(){
+        CellPosition pos = PlayerList.get(PlayerListIndex).getPos();
+        if(board.get(pos) != '-' || board.get(pos) != PlayerList.get(PlayerListIndex).getPlayerID()){//funker dette
+            playerOnNextTile();
+        }
+        return false;
+        
+    }
+
+    public void cellOcipied(){
+        CellPosition pos = PlayerList.get(PlayerListIndex).getPos();
+        if(board.get(pos) != '-' && PlayerList.get(PlayerListIndex).getPlayerID() != PlayerList.get(PlayerListIndex).getPlayerID()){//eller seg selv
+            //hopper over den cellen, men teller det som et hopp på terningen
+        }
+    } 
+
     @Override
     public GameInfo getGameInfo() {
         return gameInfo;
